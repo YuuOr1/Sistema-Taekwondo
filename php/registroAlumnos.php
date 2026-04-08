14728ed9d858e4e060f3a3e438aa05d58c650cf1
x0ttik <paul.toraya@gmail.com>
2026-04-07 20:33:30 -0700

    Se agrega la propiedad name para el formulario de registro alumno y se crea el archivo php del mismo

--- php/registroAlumnos.php
+++ php/registroAlumnos.php
@@ -1,6 +1,18 @@
 <?php
-include 'conexion.php'
+require 'conexion.php';
 
+$nombre = $_POST['nombre'];
+$fechaNac = $_POST['fechaNac'];
+$edad = $_POST['edad'];
+$ocupacion = $_POST['ocupacion'];
+$opcionEstado = $_POST['opcionEstado'];
+$fechaIngreso = $_POST['fechaIngreso'];
+$fechaExamen = $_POST['fechaExamen'];
+$opcionCinta = $_POST['opcionCinta'];
+$sigCinta = $_POST['sigCinta'];
+$telefono = $_POST['telefono'];
+$correo = $_POST['correo'];
+$empresa = $_POST['empresa'];
 
 
 ?>
